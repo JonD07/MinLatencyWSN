b380226661c7f6d987f4e757874a5667de1c643a
Jonathan Diller <jonathan.a.diller@gmail.com>
2022-05-09 09:36:02 -0600

    Updated file gen, added experiment files

--- test/Generate_Files.py
+++ test/Generate_Files.py
@@ -1,7 +1,6 @@
 import random
 
 NUM_PLOTS = 10
-NUM_SENSORS = 10
 START_COUNT = 5
 END_COUNT = 50
 Increment = 5
@@ -17,10 +16,10 @@ for n in range(START_COUNT, (END_COUNT + Increment), Increment):
 	for i in range(NUM_PLOTS):
 		file_name = f"{FILE_PATH}plot_{n}_{i}.txt"
 		with open(file_name, 'w') as file:
-			file.write(f"{NUM_SENSORS}\n")
+			file.write(f"{n}\n")
 			x_b = MAX_X * random.random()
 			y_b = MAX_Y * random.random()
-			for l in range(NUM_SENSORS):
+			for l in range(n):
 				x = MAX_X * random.random()
 				y = MAX_Y * random.random()
 				r = (R_MAX - R_MIN) * random.random() + R_MIN
